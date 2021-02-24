48fb23c5d3da7bd9b24f6576d300fc493019ba72
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-24 18:22:58 -0500

    Added counter and updated fonts

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function() {
+    // --- our code goes here ---
+    console.log("hi I'm not here!");
+    
+  $("#tweet-text").keyup(function() {
+    const tweet = $(this).val();
+    $(".counter").html(140 - tweet.length)
+  });
+});
--- server/index.js
+++ server/index.js
@@ -29,6 +29,14 @@ require("./lib/date-adjust")();
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
+app.post("/tweets", (req, res) => {
+  console.log("New tweet incoming!");
+  res.redirect("/");
+});
+
+
+
+
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
