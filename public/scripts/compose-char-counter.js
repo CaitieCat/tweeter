b449235fd2b31467f7c8939312fb0bb593622e58
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-25 15:01:17 -0500

    Character counter functions by counting down from 140 and turning red after 0

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("hi I'm not here!");
     
   $("#tweet-text").keyup(function() {
     const tweet = $(this).val();
-    $(".counter").html(140 - tweet.length);
+   $(".counter").html(140 - tweet.length);
+    console.log(tweet.length);
+   if (tweet.length > 140){
+    $(".counter").css("color", "red");
+   } else if (tweet.length <= 140) {
+    $(".counter").css("color", "#545149");
+   }
   });
 });
